a79e32e9949779fd6d06117d17f5c48233de8314
nkinta1978 <nkinta1978@gmail.com>
2016-03-28 02:12:35 +0900

    add virtual game pad

--- BluetoothSbc/Application/src/main/java/com/nkinta_pu/camera_sbgc_controller/control/PagerAdapter.java
+++ BluetoothSbc/Application/src/main/java/com/nkinta_pu/camera_sbgc_controller/control/PagerAdapter.java
@@ -3,9 +3,12 @@ package com.nkinta_pu.camera_sbgc_controller.control;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.MotionEvent;
 
 public class PagerAdapter extends FragmentStatePagerAdapter {
+
     FragmentManager mFm;
+
     public PagerAdapter(FragmentManager fm) {
         super(fm);
         mFm = fm;
@@ -26,18 +29,22 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
                 return new AutoShutterFragment();
             case 2:
                 return new GamePadFragment();
+            case 3:
+                return new VirtualGamePadFragment();
             default:
                 return new MessageFragment();
+
         }
     }
 
     @Override
     public int getCount() {
-        return 4;
+        return 5;
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
         return "Page " + position;
     }
+
 }
---
+++ BluetoothSbc/Application/src/main/java/com/nkinta_pu/camera_sbgc_controller/control/VirtualGamePadFragment.java
@@ -0,0 +1,98 @@
+package com.nkinta_pu.camera_sbgc_controller.control;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.nkinta_pu.camera_sbgc_controller.R;
+
+/**
+ * Created by NK on 2016/03/28.
+ */
+public class VirtualGamePadFragment extends ControllerFragment {
+
+    RelativeLayout layout_joystick;
+    ImageView image_joystick, image_border;
+    TextView textView1, textView2, textView3, textView4, textView5;
+
+    JoyStickClass js;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_virtual_game_pad, null);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        textView1 = (TextView)view.findViewById(R.id.textView1);
+        textView2 = (TextView)view.findViewById(R.id.textView2);
+        textView3 = (TextView)view.findViewById(R.id.textView3);
+        textView4 = (TextView)view.findViewById(R.id.textView4);
+        textView5 = (TextView)view.findViewById(R.id.textView5);
+
+        layout_joystick = (RelativeLayout)view.findViewById(R.id.layout_joystick);
+
+        js = new JoyStickClass(getActivity().getApplicationContext(), layout_joystick, R.drawable.icon);
+        js.setStickSize(150, 150);
+        js.setLayoutSize(500, 500);
+        js.setLayoutAlpha(150);
+        js.setStickAlpha(100);
+        js.setOffset(90);
+        js.setMinimumDistance(50);
+
+        layout_joystick.setOnTouchListener(new OnTouchListener() {
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                js.drawStick(arg1);
+                if(arg1.getAction() == MotionEvent.ACTION_DOWN
+                        || arg1.getAction() == MotionEvent.ACTION_MOVE) {
+                    textView1.setText("X : " + String.valueOf(js.getX()));
+                    textView2.setText("Y : " + String.valueOf(js.getY()));
+                    textView3.setText("Angle : " + String.valueOf(js.getAngle()));
+                    textView4.setText("Distance : " + String.valueOf(js.getDistance()));
+
+                    int direction = js.get8Direction();
+                    if(direction == JoyStickClass.STICK_UP) {
+                        textView5.setText("Direction : Up");
+                    } else if(direction == JoyStickClass.STICK_UPRIGHT) {
+                        textView5.setText("Direction : Up Right");
+                    } else if(direction == JoyStickClass.STICK_RIGHT) {
+                        textView5.setText("Direction : Right");
+                    } else if(direction == JoyStickClass.STICK_DOWNRIGHT) {
+                        textView5.setText("Direction : Down Right");
+                    } else if(direction == JoyStickClass.STICK_DOWN) {
+                        textView5.setText("Direction : Down");
+                    } else if(direction == JoyStickClass.STICK_DOWNLEFT) {
+                        textView5.setText("Direction : Down Left");
+                    } else if(direction == JoyStickClass.STICK_LEFT) {
+                        textView5.setText("Direction : Left");
+                    } else if(direction == JoyStickClass.STICK_UPLEFT) {
+                        textView5.setText("Direction : Up Left");
+                    } else if(direction == JoyStickClass.STICK_NONE) {
+                        textView5.setText("Direction : Center");
+                    }
+                } else if(arg1.getAction() == MotionEvent.ACTION_UP) {
+                    textView1.setText("X :");
+                    textView2.setText("Y :");
+                    textView3.setText("Angle :");
+                    textView4.setText("Distance :");
+                    textView5.setText("Direction :");
+                }
+                return true;
+            }
+        });
+    }
+
+}
+
